1e33f9512f40433e93419049a83dc7c12a1aca08
Precious Addy <162835412+Araihda@users.noreply.github.com>
2024-12-19 00:24:09 +0000

    Update Book.java

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,26 @@
 public class Book {
 
     // This is your working class.
+    private String title;
+    private String author;
+    private String publicationDate;
+
+    public Book(String title, String author, String publicationDate){
+        this.title = title;
+        this.author = author;
+        this.publicationDate = publicationDate;
+        
+    }
+
+        public String getTitle() {
+            return title;
+        }
     
+        public String getAuthor() {
+            return author;
+        }
+    
+        public String getPublicationDate() {
+            return publicationDate;
+        }
 }
